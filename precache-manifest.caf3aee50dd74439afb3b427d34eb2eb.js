8c934be4726a43fad732733425fe353e118af753
Deepak3211 <deepakt754@gmail.com>
2020-09-28 01:36:55 +0530

    Updates

---
+++ precache-manifest.caf3aee50dd74439afb3b427d34eb2eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f0dc670adad1656012b268e2984c65",
+    "url": "/robo-cards/index.html"
+  },
+  {
+    "revision": "c67ae880875d24787718",
+    "url": "/robo-cards/static/css/main.da6c82da.chunk.css"
+  },
+  {
+    "revision": "ad71fae190c548c3708d",
+    "url": "/robo-cards/static/js/2.8508b8bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-cards/static/js/2.8508b8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67ae880875d24787718",
+    "url": "/robo-cards/static/js/main.6c1ba92a.chunk.js"
+  },
+  {
+    "revision": "82bc5877a102ef0e5d78",
+    "url": "/robo-cards/static/js/runtime-main.55f78d7e.js"
+  }
+]);
